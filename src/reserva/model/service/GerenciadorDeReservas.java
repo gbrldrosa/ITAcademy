94650e37e0158d467db84f1ed44e4d717f0f38be
gbrldrosa <gabrieldrosa21@gmail.com>
2023-08-29 21:24:02 -0300

    :sparkles: Criando a lógica de reservar assento

--- src/reserva/model/service/GerenciadorDeReservas.java
+++ src/reserva/model/service/GerenciadorDeReservas.java
@@ -1,11 +1,13 @@
 package reserva.model.service;
 
+import reserva.model.Assento;
 import reserva.model.InicioReserva;
 import reserva.model.Rota;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GerenciadorDeReservas {
 
@@ -83,17 +85,44 @@ public class GerenciadorDeReservas {
         System.out.println(stringBuilderMenuRota.toString());
 
         Rota rotaSelecionada = null;
-        do{
+        do {
             opcao = scannerInput();
-            if (opcao <= 0 || opcao > rotas.size()){
+            if (opcao <= 0 || opcao > rotas.size()) {
                 System.out.println("opcao invalida tente novamente!");
             } else {
-                rotaSelecionada = rotas.get(opcao -1);
+                rotaSelecionada = rotas.get(opcao - 1);
                 System.out.println(exibeDados(rotaSelecionada));
             }
         } while (rotaSelecionada == null);
 
+        System.out.println("Digite a quantidade de reservas: (Digite apenas numeros!");
 
+        int quantidadeReservas = Integer.parseInt(scanner.nextLine());
+
+        List<Assento> assentosDisponiveis = rotaSelecionada.getListaAssentos().stream().filter(Assento::isDisponivel).collect(Collectors.toList());
+
+        List<Integer> numeroAssentosReservar = getListaNumeroAsssentosReserva(quantidadeReservas, assentosDisponiveis);
+    }
+
+    private List<Integer> getListaNumeroAsssentosReserva(int quantidadeDesejadaReserva, List<Assento> assentos) {
+
+        List<Integer> sequenciaDisponivel = new ArrayList<Integer>();
+
+        int indiceAssentoInicial = 0;
+        int count = 0;
+
+        do {
+            Assento assento = assentos.get(indiceAssentoInicial);
+            int assentoEsperado = sequenciaDisponivel.size() > 0 ? sequenciaDisponivel.get(sequenciaDisponivel.size() - 1) + 1 : assento.getNumero();
+            if (sequenciaDisponivel.size() == 0 || assento.getNumero() == assentoEsperado) {
+                sequenciaDisponivel.add(assento.getNumero());
+            } else {
+                sequenciaDisponivel.clear();
+            }
+            indiceAssentoInicial = count++;
+        } while (quantidadeDesejadaReserva > sequenciaDisponivel.size() && count <= assentos.size());
+
+        return sequenciaDisponivel;
     }
 
     private String exibeDados(Rota rota) {
